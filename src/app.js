5bc103e078462d3023664033d9f986b281183b91
Abhicoding <shinganeabhishek@gmail.com>
2018-10-12 00:43:12 +0530

    added geekdom url

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // @jsx h
-import {render, h, Component} from 'geekdom'
+import { render, h, Component } from 'geekdom'
 
 import Button from './button.js'
 export default class App extends Component {
@@ -10,28 +10,28 @@ export default class App extends Component {
       errorStatus: false,
       errorValue: ''
     }
-    this.enterOne=this.enterOne.bind(this)
-    this.enterTwo=this.enterTwo.bind(this)
-    this.enterThree=this.enterThree.bind(this)
-    this.enterFour=this.enterFour.bind(this)
-    this.enterFive=this.enterFive.bind(this)
-    this.enterSix=this.enterSix.bind(this)
-    this.enterSeven=this.enterSeven.bind(this)
-    this.enterEight=this.enterEight.bind(this)
-    this.enterNine=this.enterNine.bind(this)
-    this.enterZero=this.enterZero.bind(this)
-    this.enterPlus=this.enterPlus.bind(this)
-    this.enterMinus=this.enterMinus.bind(this)
-    this.enterMultiply=this.enterMultiply.bind(this)
-    this.enterDivide=this.enterDivide.bind(this)
-    this.enterEqual=this.enterEqual.bind(this)
-    this.enterClear=this.enterClear.bind(this)
+    this.enterOne = this.enterOne.bind(this)
+    this.enterTwo = this.enterTwo.bind(this)
+    this.enterThree = this.enterThree.bind(this)
+    this.enterFour = this.enterFour.bind(this)
+    this.enterFive = this.enterFive.bind(this)
+    this.enterSix = this.enterSix.bind(this)
+    this.enterSeven = this.enterSeven.bind(this)
+    this.enterEight = this.enterEight.bind(this)
+    this.enterNine = this.enterNine.bind(this)
+    this.enterZero = this.enterZero.bind(this)
+    this.enterPlus = this.enterPlus.bind(this)
+    this.enterMinus = this.enterMinus.bind(this)
+    this.enterMultiply = this.enterMultiply.bind(this)
+    this.enterDivide = this.enterDivide.bind(this)
+    this.enterEqual = this.enterEqual.bind(this)
+    this.enterClear = this.enterClear.bind(this)
   }
 
   errorFunc (str) {
-    this.setState({errorValue: str})
+    this.setState({ errorValue: str })
     this.errorStatusFunc(true)
-    setTimeout(() => {this.errorStatusFunc(false), 3000})
+    setTimeout(() => { this.errorStatusFunc(false), 3000 })
   }
 
   errorStatusFunc (boolean) {
@@ -125,7 +125,7 @@ export default class App extends Component {
   enterEqual () {
     var x
     try {
-      var x = eval (this.state.screen)
+      var x = eval(this.state.screen)
       return this.setState({
         screen: x.toString()
       })
@@ -146,22 +146,42 @@ export default class App extends Component {
         <h1 class='header'>GeekCalc</h1>
         <div class='screen'><div>{this.state.screen}</div></div>
         <div class='grid-container-50-half'>
-          <Button class='item one' clickHandler={this.enterOne} value={1}/>
-          <Button class='item two' clickHandler={this.enterTwo} value={2}/>
-          <Button class='item three' clickHandler={this.enterThree} value={3}/>
-          <Button class='item plus' clickHandler={this.enterPlus} value={'+'}/>
-          <Button class='item four' clickHandler={this.enterFour} value={4}/>
-          <Button class='item five' clickHandler={this.enterFive} value={5}/>
-          <Button class='item six' clickHandler={this.enterSix} value={6}/>
-          <Button class='item minus' clickHandler={this.enterMinus} value={'-'}/>
-          <Button class='item seven' clickHandler={this.enterSeven} value={7}/>
-          <Button class='item eight' clickHandler={this.enterEight} value={8}/>
-          <Button class='item nine' clickHandler={this.enterNine} value={9}/>
-          <Button class='item multiply' clickHandler={this.enterMultiply} value={'*'}/>
-          <Button class='item zero' clickHandler={this.enterZero} value={0}/>
-          <Button class='item equal' clickHandler={this.enterEqual} value={'='}/>
-          <Button class='item clear' clickHandler={this.enterClear} value={'C'}/>
-          <Button class='item divide' clickHandler={this.enterDivide} value={'/'}/>
+          <Button class='item one'
+            clickHandler={this.enterOne} value={1} />
+          <Button class='item two'
+            clickHandler={this.enterTwo} value={2} />
+          <Button class='item three'
+            clickHandler={this.enterThree} value={3} />
+          <Button class='item plus'
+            clickHandler={this.enterPlus} value={'+'} />
+          <Button class='item four'
+            clickHandler={this.enterFour} value={4} />
+          <Button class='item five'
+            clickHandler={this.enterFive} value={5} />
+          <Button class='item six'
+            clickHandler={this.enterSix} value={6} />
+          <Button class='item minus'
+            clickHandler={this.enterMinus} value={'-'} />
+          <Button class='item seven'
+            clickHandler={this.enterSeven} value={7} />
+          <Button class='item eight'
+            clickHandler={this.enterEight} value={8} />
+          <Button class='item nine'
+            clickHandler={this.enterNine} value={9} />
+          <Button class='item multiply'
+            clickHandler={this.enterMultiply} value={'*'} />
+          <Button class='item zero'
+            clickHandler={this.enterZero} value={0} />
+          <Button class='item equal'
+            clickHandler={this.enterEqual} value={'='} />
+          <Button class='item clear'
+            clickHandler={this.enterClear} value={'C'} />
+          <Button class='item divide'
+            clickHandler={this.enterDivide} value={'/'} />
+        </div>
+        <div class='footer'>Built using &nbsp;
+          <a href='https://github.com/Abhicoding/geekDOM' 
+            target='_blank'>geekdom</a>
         </div>
       </div>
     )
